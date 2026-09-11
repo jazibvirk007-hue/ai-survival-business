7c2a944ff04af07f32b5f3ad9fdb82174a25af55
jazibvirk007-hue <jazib.virk007@gmail.com>
2026-09-11 21:07:19 +0400

    Add one-click Cortex launcher

---
+++ Start-Cortex.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
+if not exist ".venv\Scripts\python.exe" (
+  echo Cortex is not installed yet. Running the installer...
+  call "%~dp0Install-Cortex.bat"
+  if errorlevel 1 exit /b 1
+)
+
+start "TJ Cortex Command Center" "%~dp0.venv\Scripts\python.exe" "%~dp0cortex_dashboard.py"
+
+timeout /t 2 /nobreak >nul
+start "" "http://127.0.0.1:8080/"
+exit /b 0
